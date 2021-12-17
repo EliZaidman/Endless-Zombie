f086f49952275506aef41345282534595a7a0b1b
Daimnon <galdrorerez@gmail.com>
2021-12-17 19:43:31 +0200

    CanShoot

--- Zombies_Gal_Zaidman_BenHaim_Vaknin/Assets/Scripts/Player Scripts/Movement/MultipleTouchController.cs
+++ Zombies_Gal_Zaidman_BenHaim_Vaknin/Assets/Scripts/Player Scripts/Movement/MultipleTouchController.cs
@@ -16,7 +16,8 @@ public class MultipleTouchController : MonoBehaviour
     [SerializeField]
     private float _speed = 6f;
 
-    private Fire _fire = new Fire();
+    [SerializeField]
+    private Fire _fire;
 
     private Vector2 _leftJoystickStartPos, _rightJoystickStartPos;
     private void Start()
@@ -46,25 +47,15 @@ public class MultipleTouchController : MonoBehaviour
                     Debug.Log("touch is moving");
                     TouchData thisTouch = _allTouches.Find(touchLocation => touchLocation.TouchId == t.fingerId);
 
-                    Vector2 offset = touchPos - thisTouch.StartTouchPos;
-                    Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
-
-                    _leftJoystickTr.transform.position = new Vector2(thisTouch.StartTouchPos.x + direction.x, thisTouch.StartTouchPos.y + direction.y);
-                    _leftJoystickBgTr.transform.position = new Vector2(thisTouch.StartTouchPos.x, thisTouch.StartTouchPos.y);
-
-                    MovePlayer(direction);
+                    LeftJystickMovement(thisTouch, touchPos);
                 }
                 else if (t.position.x > Screen.width / 2)
                 {
                     Debug.Log("touch is moving");
                     TouchData thisTouch = _allTouches.Find(touchLocation => touchLocation.TouchId == t.fingerId);
 
-                    Vector2 offset = touchPos - thisTouch.StartTouchPos;
-                    Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
-
-                    _rightJoystickTr.transform.position = new Vector2(thisTouch.StartTouchPos.x + direction.x, thisTouch.StartTouchPos.y + direction.y);
-                    _rightJoystickBgTr.transform.position = new Vector2(thisTouch.StartTouchPos.x, thisTouch.StartTouchPos.y);
-                    //Shoot(true);
+                    RightJystickMovement(thisTouch, touchPos);
+                    Shoot(true);
                 }
             }
             else if (t.phase == TouchPhase.Ended)
@@ -74,8 +65,7 @@ public class MultipleTouchController : MonoBehaviour
                     Debug.Log("touch is moving");
                     TouchData thisTouch = _allTouches.Find(touchLocation => touchLocation.TouchId == t.fingerId);
 
-                    _leftJoystickTr.transform.position = new Vector2(_leftJoystickStartPos.x, _leftJoystickStartPos.y);
-                    _leftJoystickBgTr.transform.position = new Vector2(_leftJoystickStartPos.x, _leftJoystickStartPos.y);
+                    ResetLeftJoystick();
 
                     _allTouches.RemoveAt(_allTouches.IndexOf(thisTouch));
                 }
@@ -84,8 +74,8 @@ public class MultipleTouchController : MonoBehaviour
                     Debug.Log("touch ended");
                     TouchData thisTouch = _allTouches.Find(touchLocation => touchLocation.TouchId == t.fingerId);
 
-                    _rightJoystickTr.transform.position = new Vector2(_rightJoystickStartPos.x, _rightJoystickStartPos.y);
-                    _rightJoystickBgTr.transform.position = new Vector2(_rightJoystickStartPos.x, _rightJoystickStartPos.y);
+                    ResetRightJoystick();
+                    Shoot(false);
 
                     _allTouches.RemoveAt(_allTouches.IndexOf(thisTouch));
                 }
@@ -99,6 +89,37 @@ public class MultipleTouchController : MonoBehaviour
         return GetComponent<Camera>().ScreenToWorldPoint(new Vector3(touchPosition.x, touchPosition.y, transform.position.z));
     }
 
+    private void LeftJystickMovement(TouchData touchdata, Vector2 touchPos)
+    {
+        Vector2 offset = touchPos - touchdata.StartTouchPos;
+        Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+
+        _leftJoystickTr.transform.position = new Vector2(touchdata.StartTouchPos.x + direction.x, touchdata.StartTouchPos.y + direction.y);
+        _leftJoystickBgTr.transform.position = new Vector2(touchdata.StartTouchPos.x, touchdata.StartTouchPos.y);
+
+        MovePlayer(direction);
+    }
+
+    private void RightJystickMovement(TouchData touchdata, Vector2 touchPos)
+    {
+        Vector2 offset = touchPos - touchdata.StartTouchPos;
+        Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+
+        _rightJoystickTr.transform.position = new Vector2(touchdata.StartTouchPos.x + direction.x, touchdata.StartTouchPos.y + direction.y);
+        _rightJoystickBgTr.transform.position = new Vector2(touchdata.StartTouchPos.x, touchdata.StartTouchPos.y);
+    }
+
+    private void ResetLeftJoystick()
+    {
+        _leftJoystickTr.transform.position = new Vector2(_leftJoystickStartPos.x, _leftJoystickStartPos.y);
+        _leftJoystickBgTr.transform.position = new Vector2(_leftJoystickStartPos.x, _leftJoystickStartPos.y);
+    }
+
+    private void ResetRightJoystick()
+    {
+        _rightJoystickTr.transform.position = new Vector2(_rightJoystickStartPos.x, _rightJoystickStartPos.y);
+        _rightJoystickBgTr.transform.position = new Vector2(_rightJoystickStartPos.x, _rightJoystickStartPos.y);
+    }
     void MovePlayer(Vector2 direction)
     {
         _playerTr.Translate(direction * _speed * Time.deltaTime);
