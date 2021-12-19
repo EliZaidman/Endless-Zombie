cf8ea5c09ff0d22400397a3ed5623906fe421047
Eli-PC <rxelik5@gmail.com>
2021-12-19 19:00:01 +0200

    Camera Follow Player

---
+++ Zombies_Gal_Zaidman_BenHaim_Vaknin/Assets/Scripts/Player Scripts/Movement/FollowPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    //Object you want to follow
+    public Transform target;
+
+    //Set to z -10
+    public Vector3 offset;
+
+    //How much delay on the follow
+    [Range(1, 10)]
+    public float smoothing;
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.fixedDeltaTime);
+        transform.position = smoothPosition;
+    }
+
+}
