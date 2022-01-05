b1f1a1598b8a13ff577583c704e769187ad0673a
Daimnon <galdrorerez@gmail.com>
2022-01-05 17:49:59 +0200

    Added Tracker

---
+++ Zombies_Gal_Zaidman_BenHaim_Vaknin/Assets/Scripts/Systems/Enemies & AI/EnemyTracker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTracker : MonoBehaviour
+{
+    public GameObject TrackerPos;
+
+    private void Start()
+    {
+        TrackerPos = GameObject.Find("TrackerPos");
+    }
+    void Update()
+    {
+        var dir = transform.parent.transform.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.position = TrackerPos.transform.position;
+    }
+}
